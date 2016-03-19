6b24dcf1606602b37cc1fa3c2123525e0a0fb198
The Tung NGUYEN <The Tung NGUYEN>
2016-03-19 18:19:55 +0100

    NameLength : refactoring

--- src/main/NameLength.ts
+++ src/main/NameLength.ts
@@ -2,24 +2,19 @@
  * NameLength * 
  */
 export class NameLength {
+        
     
     returnNameAndLength(names:string[]):[string, number][] {
         return names.map((name):[string, number] => [name, name.length]);
     }
     
     displayNameAndLength(names:string[], literal:boolean = false) {
-        enum NumberToString {'un', 'deux', 'trois', 'quatre', 'cinq', 'six', 'sept', 'huit', 'neuf'};
+        enum NumberToString {un, deux, trois, quatre, cinq, six, sept, huit, neuf};    
         
         this.returnNameAndLength(names)
-        .filter(([name, length]: [string, number]):boolean => length <= 9)
-        .forEach((nameAndLength:[string, number]) => {
-                var [name, length] = nameAndLength;
-                                           
-                if (literal) {
-                    console.log(`${name} contient ${NumberToString[length - 1]} caractères`);
-                } else {
-                    console.log(`${name} contient ${length} caractères`);
-                }
-        });    
+            .filter(([name, length]: [string, number]):boolean => length <= 9)
+            .forEach(([name, length]:[string, number]) => {
+                console.log(`${name} contient ${literal ? NumberToString[length - 1] : length} caractères`);
+            });    
     }
 }
